87e1e1b3dd13e1a8f134351f2989fb992ebabc80
nealhartley <neal.w.hartley@gmail.com>
2017-12-11 18:15:15 +1300

    Set up the initial CityService framework

--- src/code/CityService.java
+++ src/code/CityService.java
@@ -1,4 +1,25 @@
 package code;
 
+import java.util.ArrayList;
+
 public class CityService {
+
+    private CityStream cs;
+    private CityFactory cf;
+
+    public CityService(){
+        cs = new CityStream();
+        cf = new CityFactory();
+    }
+
+    public ArrayList<CityFactory> fetchCities(){
+        //set up cs
+
+
+        //loop through cs return values to create cities w factory
+
+        //return list of Cities
+        return null; // temp
+    }
+
 }
